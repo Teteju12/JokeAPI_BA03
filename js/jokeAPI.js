1bc5cd52fbf5adc7a6aad958987068807e8ee736
Teteju12 <jmaldonado.gamedesign@gmail.com>
2021-05-13 19:17:05 +0200

    added the weather API

--- js/jokeAPI.js
+++ js/jokeAPI.js
@@ -1,14 +1,14 @@
-/*
-function NextJoke(){
-  GetJoke();
-}
-*/
-const urlAPI = 'https://icanhazdadjoke.com/';
+
+const urlJokeAPI = 'https://icanhazdadjoke.com/';
+const urlWeatherAPI = 'https://www.el-tiempo.net/api/json/v2/home'
 const html_text = document.getElementById('t_dadJoke');
+const html_weather = document.getElementById('t_WeatherReport');
+let weathercity = "Barcelona";
+GetLocalWeather();
 
 
 async function GetJoke(){
-  let joke = await fetch(urlAPI, {
+  let joke = await fetch(urlJokeAPI, {
     method: "GET",
     headers: {Accept: 'application/json'}
   })
@@ -20,7 +20,24 @@ async function GetJoke(){
     }
   })
   .catch((response)=>{
-    console.error(response.status + " returned. Error fetching " + urlAPI);
-  })
+    console.error(response.status + " returned. Error fetching " + urlJokeAPI);
+  });
 
+}
+
+async function GetLocalWeather(){
+  let weather = await fetch(urlWeatherAPI)
+  .then((response) => {
+    response.json().then((data) =>{
+      let cities = data.ciudades;
+      for(var i = 0; i < cities.length; i++){
+        if(cities[i].name == weathercity){
+          html_weather.innerHTML = "today in " + cities[i].name + ": " + cities[i].stateSky.description;
+        }
+      }
+    })
+  })
+  .catch((response)=>{
+    console.error(response.status + " returned. Error fetching " + urlWeatherAPI);
+  });
 }
